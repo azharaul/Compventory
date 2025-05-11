20b436d100c5691fb7436e42530e9b3233fbdeaa
yandevxxx <157345247+yandevxxx@users.noreply.github.com>
2025-05-11 16:40:15 +0700

    Update Hash & Check akun

--- src/app/Akun.java
+++ src/app/Akun.java
@@ -11,6 +11,9 @@ package app;
 import java.sql.*;
 import static javax.swing.JOptionPane.ERROR_MESSAGE;
 import static javax.swing.JOptionPane.showMessageDialog;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import javax.swing.JOptionPane;
 
 interface AkunInterface {
     boolean register(String username, String password, String role);
@@ -28,7 +31,7 @@ public class Akun implements AkunInterface {
         String query = "INSERT INTO akun (username, password, role) VALUES (?, ?, ?)";
         try (Connection conn = DBConnection.getConnection(); PreparedStatement stmt = conn.prepareStatement(query)) {
             stmt.setString(1, username);
-            stmt.setString(2, password);
+            stmt.setString(2, hashPassword(password));
             stmt.setString(3, role);
             int affected = stmt.executeUpdate();
             return affected > 0;
@@ -38,12 +41,42 @@ public class Akun implements AkunInterface {
         }
     }
 
+    public boolean isUsernameTaken(String username) {
+        String query = "SELECT COUNT(*) FROM akun WHERE username = ?";
+        try (Connection conn = DBConnection.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(query)) {
+
+            stmt.setString(1, username);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                return rs.getInt(1) > 0;
+            }
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage(), "Registration", ERROR_MESSAGE);
+        }
+        return false;
+    }
+
+    public String hashPassword(String password) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            byte[] hashedBytes = md.digest(password.getBytes());
+            StringBuilder hexString = new StringBuilder();
+            for (byte b : hashedBytes) {
+                hexString.append(String.format("%02x", b));
+            }
+            return hexString.toString();
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("Hashing failed", e);
+        }
+    }
+    
     @Override
     public boolean login(String username, String password) {
         String query = "SELECT * FROM akun WHERE username = ? AND password = ?";
         try (Connection conn = DBConnection.getConnection(); PreparedStatement stmt = conn.prepareStatement(query)) {
             stmt.setString(1, username);
-            stmt.setString(2, password);
+            stmt.setString(2, hashPassword(password));
             ResultSet rs = stmt.executeQuery();
             return rs.next();
         } catch (SQLException e) {
